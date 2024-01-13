971980ca73d67050d83f77844509543cb3989b4e
Rida <63632471+rfaraz@users.noreply.github.com>
2024-01-13 01:22:00 -0800

    Latest version

--- sarcasm_detection.py
+++ sarcasm_detection.py
@@ -131,7 +131,7 @@ for _ in trange(epochs, desc="Epoch"):
     batch = tuple(t.to(device) for t in batch)
     # Unpack the inputs from our dataloader
     b_input_ids, b_input_mask, b_labels = batch
-    # Clear out the gradients (by default they accumulate)
+    # Clear out the gradients
     optimizer.zero_grad()
     # Forward pass
     outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
@@ -165,13 +165,12 @@ for _ in trange(epochs, desc="Epoch"):
     batch = tuple(t.to(device) for t in batch)
     # Unpack the inputs from our dataloader
     b_input_ids, b_input_mask, b_labels = batch
-    # Telling the model not to compute or store gradients, saving memory and speeding up validation
+    # Telling the model not to compute or store gradients
     with torch.no_grad():
-      # Forward pass, calculate logit predictions
+      # Forward pass, calculate predictions
       output = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
       logits = output[0]
 
-    # Move logits and labels to CPU
     logits = logits.detach().cpu().numpy()
     label_ids = b_labels.to('cpu').numpy()
 
